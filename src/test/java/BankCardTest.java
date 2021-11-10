f0635711dffb05e15e80893e6e1521b9a8cd90d9
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-11-10 10:26:06 +0300

    4 tests run, 2 failed, 2 passed

--- src/test/java/BankCardTest.java
+++ src/test/java/BankCardTest.java
@@ -1,17 +1,23 @@
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class BankCardTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
 
     @Test
     void shouldTestName(){
 
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("Petr Vasiliev");
         form.$("[data-test-id=phone] input").setValue("+79162875634");
@@ -24,7 +30,6 @@ class BankCardTest {
         @Test
     void shouldTestNameBlank(){
 
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+79162875634");
@@ -36,25 +41,23 @@ class BankCardTest {
     @Test
     void shouldTestTel(){
 
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("Яша Пупкин");
         form.$("[data-test-id=phone] input").setValue("89992223344");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button]").click();
-        $$("[class=input__sub]").shouldHave(CollectionCondition.exactTexts("Укажите точно как в паспорте, Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-        }
+        $("[class=input] input_invalid").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+      }
 
-    @Test
+
+      @Test
     void shouldTestTelBlank(){
 
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("Яша Пупкин");
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button]").click();
-        $$("[class=input__sub]").shouldHave(CollectionCondition.exactTexts("Укажите точно как в паспорте, Поле обязательно для заполнения"));
+        $("[type=text view=default] input_invalid").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
-
 }
