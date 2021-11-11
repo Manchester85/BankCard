96e48cca1552d31026ece18ad805c46e555f85e9
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-11-11 09:46:29 +0300

    all tests are passed

--- src/test/java/BankCardTest.java
+++ src/test/java/BankCardTest.java
@@ -16,48 +16,59 @@ class BankCardTest {
     }
 
     @Test
-    void shouldTestName(){
+    void shouldTestName() {
 
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("Petr Vasiliev");
         form.$("[data-test-id=phone] input").setValue("+79162875634");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button]").click();
-        $("[class=input__sub]").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. " +
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. " +
                 "Допустимы только русские буквы, пробелы и дефисы."));
-        }
+    }
 
-        @Test
-    void shouldTestNameBlank(){
+    @Test
+    void shouldTestNameBlank() {
 
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+79162875634");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button]").click();
-        $("[class=input__sub]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void shouldTestTel(){
+    void shouldTestTel() {
 
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("Яша Пупкин");
         form.$("[data-test-id=phone] input").setValue("89992223344");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button]").click();
-        $("[class=input] input_invalid").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-      }
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 
 
-      @Test
-    void shouldTestTelBlank(){
+    @Test
+    void shouldTestTelBlank() {
 
         SelenideElement form = $("[action]");
         form.$("[data-test-id = name] input").setValue("Яша Пупкин");
         form.$("[data-test-id=phone] input").setValue("");
         form.$("[data-test-id=agreement] .checkbox__box").click();
         form.$("[role=button]").click();
-        $("[type=tel view=default] input_invalid").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestCheckbox() {
+
+        SelenideElement form = $("[action]");
+        form.$("[data-test-id = name] input").setValue("Яша Пупкин");
+        form.$("[data-test-id=phone] input").setValue("+79867775544");
+        form.$("[data-test-id=agreement] .checkbox__box");
+        form.$("[role=button]").click();
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
